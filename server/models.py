673e433690d880e00a8f0f4be93e75e5877d048c
Tess <tess.mueske@gmail.com>
2024-09-23 19:42:55 -0700

    column edits, model additions

--- server/models.py
+++ server/models.py
@@ -3,13 +3,13 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# Models go here!
-
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False)
+
+    periods = db.relationship('Period', back_populates='user')
 
     def __repr__(self):
         return f'<{self.id}: {self.username}>'
@@ -21,11 +21,23 @@ class Period(db.Model, SerializerMixin):
     start_date = db.Column(db.Date)
     end_date = db.Column(db.Date)
 
+    symptoms = db.relationship('Symptom', secondary='periodsymptoms', back_populates='periods')
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return f'Period <{self.id}: {self.start_date} through {self.end_date}>'
+
 class Symptom(db.Model, SerializerMixin):
     __tablename__ = 'symptoms'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+
+    periods = db.relationship('Period', secondary='periodsymptoms', back_populates='symptoms')
+
+    def __repr__(self):
+        return f'Symptom <{self.id}: {self.name}>'
 
 class PeriodSymptom(db.Model, SerializerMixin):
     __tablename__ = 'periodsymptoms'
@@ -33,6 +45,9 @@ class PeriodSymptom(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     severity = db.Column(db.String)
     notes = db.Column(db.String)
+
+    period_id = db.Column(db.Integer, db.ForeignKey('periods.id'))
+    symptom_id = db.Column(db.Integer, db.ForeignKey('symptoms.id'))
 
 
 # Import your models as needed in other modules, i.e. from models import ....
