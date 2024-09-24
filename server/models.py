ca4bdb658228df75b48494efc6c24a5fb768ffc8
Tess <tess.mueske@gmail.com>
2024-09-23 19:17:53 -0700

    adding columns to tables

--- server/models.py
+++ server/models.py
@@ -6,19 +6,36 @@ from config import db
 # Models go here!
 
 class User(db.Model, SerializerMixin):
-    return ''
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f'<{self.id}: {self.username}>'
 
 class Period(db.Model, SerializerMixin):
-    return'
+    __tablename__ = 'periods'
+
+    id = db.Column(db.Integer, primary_key=True)
+    start_date = db.Column(db.Date)
+    end_date = db.Column(db.Date)
 
 class Symptom(db.Model, SerializerMixin):
-    return ''
+    __tablename__ = 'symptoms'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
 
 class PeriodSymptom(db.Model, SerializerMixin):
-    return ''
+    __tablename__ = 'periodsymptoms'
+
+    id = db.Column(db.Integer, primary_key=True)
+    severity = db.Column(db.String)
+    notes = db.Column(db.String)
 
 
-# Edit models.py and start creating your models. Import your models as needed in other modules, i.e. from models import ....
+# Import your models as needed in other modules, i.e. from models import ....
 
 # Remember to regularly run flask db revision --autogenerate -m'<descriptive message>', replacing <descriptive message> with an appropriate message, and flask db upgrade head to track your modifications to the database and create checkpoints in case you ever need to roll those modifications back.
 
