b291b7ad542fcc37e019383ff0de43e7a2159ed1
Tess <tess.mueske@gmail.com>
2024-09-26 13:11:36 -0700

    adding more components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,12 +4,30 @@ import { Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div>
+      {/* home page */}
+      <Home />
+      {/* about page */}
+      <About /> 
+      {/* navigation */}
+      <NavBar /> 
+      {/* create account page */}
+      <CreateAccount /> 
       {/* login page */}
       <Login /> 
-      {/* home landing page */}
+      {/* home landing page AFTER login */}
       <Homepage />
       {/* form for adding a period */} 
       <AddPeriod /> 
+      {/* form for adding a symptom */}
+      <AddSymptom /> 
+      {/* viewing all periods, has calendar scroll function */}
+      <ViewAllPeriods /> 
+      {/* once a period is selected, options here */}
+      <SelectedPeriod /> 
+      {/* after new period is added, landing page with new options */}
+      <NowWhat /> 
+      {/* after new symptom is added, landing page with new options */}
+      <NowWhat2 /> 
     </div>
   );
 }
