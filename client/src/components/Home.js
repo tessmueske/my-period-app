39fbf1935605fcdbb2acc8c526f966c42c4c0a42
Tess <tess.mueske@gmail.com>
2024-09-27 20:55:15 -0700

    styling for landing page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,11 +5,15 @@ import '../index.css';
 function Home() {
     return (
         <>
-            <h2>welcome to Crimson, </h2>
-            <h2>an app for all people who menstruate</h2><br></br>
-            <Link to="/login">log in</Link><br></br>
-            <Link to="/signup">sign up</Link><br></br>
-            <Link to="/about">about us</Link><br></br>
+            <h2>welcome to Crimson,</h2>
+            <h2>an app for all people who menstruate</h2>
+            <br />
+            <Link to="/login" className="button">log in</Link>
+            <br />
+            <Link to="/signup" className="button">sign up</Link>
+            <br />
+            <Link to="/about" className="button">about us</Link>
+            <br />
         </>
     );
 }
